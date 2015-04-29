774d75ece440d25faf264fe78b93ea210966f80f
gxc- <gabrielchow93@gmail.com>
2015-04-28 22:55:06 -0700

    Started project.

--- app/src/main/java/com/snapstone/gabrielchow/snapstone/MainActivity.java
+++ app/src/main/java/com/snapstone/gabrielchow/snapstone/MainActivity.java
@@ -4,14 +4,38 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.widget.Button;
+import android.view.View;
+import android.view.View.OnClickListener;
 
 
 public class MainActivity extends ActionBarActivity {
+
+    Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void addListenerOnButton(){
+
+        final Context context = this;
+
+        button = (Button) findViewById(R.id.button1);
+
+        button.setOnClickListener(new OnClickListener(){
+
+            @Override
+            public void onClick(View arg0) {
+                Intent intent = new Intent(context, CameraActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 
 
